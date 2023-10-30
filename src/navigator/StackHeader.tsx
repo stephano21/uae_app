5e3e72988fe5d6a7c7708c60805e5ceb4b22e75a
Walter Celi <antonini2903@hotmail.com>
2023-10-29 20:10:59 -0500

    Added suppert for action buttins in action bar

--- src/navigator/StackHeader.tsx
+++ src/navigator/StackHeader.tsx
@@ -6,9 +6,10 @@ import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   title: string;
+  actions?: React.JSX.Element[];
 }
 
-export const StackHeader = ({title}: Props) => {
+export const StackHeader = ({title, actions}: Props) => {
   const navigation = useNavigation();
 
   return (
@@ -24,11 +25,16 @@ export const StackHeader = ({title}: Props) => {
       <TouchableOpacity
         activeOpacity={0.6}
         onPress={() => navigation.goBack()}
-        style={{height: 50, width: 50, ...styles.centerItems}}>
+        style={{height: 50, width: 50, ...styles.centerItems}}
+      >
         <Icon name={iconos.atras} size={30} color={colores.blanco} />
       </TouchableOpacity>
       <Text style={{...styles.textTitle}}>{title}</Text>
-      <View style={{height: 50, width: 50}}></View>
+      <View style={{maxHeight: 50}}>
+        {actions && actions?.length > 0
+          && actions?.map((action, i) => <View key={i} style={{...styles.centerItems}}>{action}</View>)
+        }
+      </View>
     </View>
   );
 };
